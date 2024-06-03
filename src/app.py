2c80a791ef90bf4e475cfa279a20905126d24a15
HungThinhNguyen27 <Thung4199@gmail.com>
2024-06-03 15:14:54 +0700

    Initial commit

---
+++ src/app.py
@@ -0,0 +1,33 @@
+
+from flask import Flask 
+
+app = Flask(__name__)
+
+@app.route("/")
+def index(): 
+    return "hello container from flask"
+
+if __name__ == '__main__':
+   app.run(debug=False, host='0.0.0.0')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# python3 -m venv env
+# source env/bin/activate
+# pip3 install flask uwsgi
+# pip freeze > requirements.txt
+# source /Users/lap01743/Downloads/WorkSpace/e-commerce_project/src/env/bin/activate
+# sudo nano /etc/hosts sữa tên miền 
